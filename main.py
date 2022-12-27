e791f5073c63c08345468d66f1b349e7a15f538e
Svetla Alexandrova <121426643+Svetla-Alexandrova@users.noreply.github.com>
2022-12-27 14:13:02 +0000

    Updated with dynamic getting of info from open API news site

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import requests
 
-r = requests.get('https://newsapi.org/v2/everything?qInTitle=stock%20market&from=2022-11-27&to=2022-12-28&sortBy=popularity&language=en&apiKey=b2ae49eb975f497f84774617a6b72971')
+# r = requests.get('https://newsapi.org/v2/everything?qInTitle=stock%20market&from=2022-11-27&to=2022-12-28&sortBy=popularity&language=en&apiKey=b2ae49eb975f497f84774617a6b72971')
 
-content = r.json()
-# print(content['articles'][0]['title'])
+# content = r.json()
+# # print(content['articles'][0]['title'])
 
-articles = content['articles']
+# articles = content['articles']
 
-for article in articles:
-  print("Title:\n", article['title'], "\nDescription:\n", article['description'])
+# for article in articles:
+#   print("Title:\n", article['title'], "\nDescription:\n", article['description'])
+
+def get_news(topic, from_date, to_date, language='en', api_key='b2ae49eb975f497f84774617a6b72971'):
+  url = f"https://newsapi.org/v2/everything?qInTitle={topic}&from={from_date}&to={to_date}&sortBy=popularity&{language}=en&apiKey={api_key}"
+  r = requests.get(url)
+  content = r.json()
+  articles = content['articles']
+
+  results = []
+  for article in articles:
+    results.append(f"TITLE:\n{article['title']}\nDESCRIPTION:\n{article['description']}")
+
+  return '\n'.join(results)
+  
+print(get_news(topic='stock', from_date='2022-12-25', to_date='2022-12-28')) 
